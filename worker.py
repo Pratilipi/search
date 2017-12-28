ee52a3d76cabcec7f4aa7965d42d43d3cf697828
gauri <gauri@pratilipi.com>
2017-12-28 17:48:35 +0530

    fixed issues related to author

--- worker.py
+++ worker.py
@@ -3,6 +3,7 @@ import ujson
 import time
 import boto3
 import solr
+import copy
 
 # setting encoding for app
 import sys
@@ -12,8 +13,8 @@ sys.setdefaultencoding('utf-8')
 # config
 SOLR_URL = "http://localhost:8983/solr"
 STAGE = "local" if "STAGE" not in os.environ else os.environ["STAGE"]
-QUEUE_URL = "https://sqs.ap-southeast-1.amazonaws.com/{}/{}-author".format(os.environ["AWS_PROJ_ID"], STAGE)
-
+QUEUE_URL = "https://sqs.ap-southeast-1.amazonaws.com/{}/{}-search".format(os.environ["AWS_PROJ_ID"], 'devo' if STAGE == 'local' else STAGE)
+POLL_SLEEP_TIME = 10
 
 class Event:
     def __init__(self):
@@ -26,43 +27,101 @@ class Event:
 class Author:
     def __init__(self, kwargs):
         """init"""
-        self.conn = solr.Solr('{}/author'.format(SOLR_URL))
+        self._conn = solr.SolrConnection('{}/author'.format(SOLR_URL))
         for name in kwargs:
-            setattr(self, name, kwargs[name])
-        print "author obj init ", self.__dict__()
+            attribute = self.transformer(name)
+            if attribute is None: continue
+            value = kwargs[name]
+            setattr(self, attribute, value)
 
+    def transformer(self, key):
+        key = key.lower().strip()
+        attribute = {}
+        attribute['authorid'] = 'author_id'
+        attribute['language'] = 'language'
+        attribute['firstname'] = 'first_name'
+        attribute['lastname'] = 'last_name'
+        attribute['firstnameen'] = 'first_name_en'
+        attribute['lastnameen'] = 'last_name_en'
+        attribute['penname'] = 'pen_name'
+        attribute['pennameen'] = 'pen_name_en'
+        attribute['summary'] = 'summary'
+        return attribute[key] if key in attribute else None
+        
     def add(self):
         """add doc"""
-        doc = self.__dict__()
-        self.conn.add(doc, commit=True)
+        doc = copy.copy(self.__dict__)
+        self._conn.add(author_id=doc['author_id'], language=doc['language'], first_name=doc['first_name'], 
+                       last_name=doc.get('last_name', None), pen_name=doc.get('pen_name', None), 
+                       first_name_en=doc.get('first_name_en', None), last_name_en=doc.get('last_name_en'), 
+                       pen_name_en=doc.get('pen_name_en', None), summary=doc.get('summary', None))
+        self._conn.commit()
         print "author added ", doc
 
     def delete(self):
         """delete doc"""
-        self.conn.delete(author_id=self.author_id, commit=True)
+        self._conn.delete_query("author_id:{}".format(self.author_id))
+        self._conn.commit()
         print "author deleted ", self.author_id
+
+    def get(self):
+        """get doc"""
+        dataset = self._conn.query("author_id:{}".format(self.author_id))
+        data = None
+        for row in dataset:
+            data = row
+        return data
 
     def update(self):
         """update doc"""
+        old_doc = self.get()
+
+        if old_doc is None:
+            self.add()
+            return
+
+        new_doc = copy.copy(self.__dict__)
+
+        for key in new_doc:
+            old_doc[key] = new_doc[key]
+        
+        self.delete()
         self.add()
+        print "author updated ", old_doc
 
 class Pratilipi:
     def __init__(self, kwargs):
         """init"""
-        self.conn = solr.Solr('{}/pratilipi'.format(SOLR_URL))
+        self._conn = solr.SolrConnection('{}/pratilipi'.format(SOLR_URL))
         for name in kwargs:
-            setattr(self, name, kwargs[name])
-        print "pratilipi obj init ", self.__dict__()
+            attribute = self.transformer(name)
+            if attribute is None: continue
+            value = kwargs[name]
+            setattr(self, attribute, value)
+
+    def transformer(key):
+        key = key.lower().strip()
+        attribute = {}
+        attribute['pratilipiid'] = 'pratilipi_id'
+        attribute['language'] = 'language'
+        attribute['authorid'] = 'author_id'
+        attribute['title'] = 'title'
+        attribute['titleen'] = 'title_en'
+        attribute['summary'] = 'summary'
+        attribute['cotenttype'] = 'content_type'
+        attribute['category'] = 'category'
+        attribute['categoryen'] = 'category_en'
+        return attribute[key] if key in attribute else None
 
     def add(self):
         """add doc"""
-        doc = self.__dict__()
-        self.conn.add(doc, commit=True)
+        doc = copy.copy(self.__dict__)
+        self._conn.add(doc, commit=True)
         print "pratilipi added ", doc
 
     def delete(self):
         """delete doc"""
-        self.conn.delete(pratilipi_id=self.pratilipi_id, commit=True)
+        self._conn.delete(pratilipi_id=self.pratilipi_id, commit=True)
         print "pratilipi deleted ", self.pratilipi_id
 
     def update(self):
@@ -78,13 +137,10 @@ class SearchQueue:
     def poll(self):
         """poll queue"""
         setattr(self, 'events', [])
-        response = self.client.receive_message( QueueUrl = self.url,
-                                                AttributeNames = [ 'SentTimestamp' ],
-                                                MaxNumberOfMessages = 10,
-                                                MessageAttributeNames = [ 'All' ],
-                                                VisibilityTimeout = 0,
-                                                WaitTimeSeconds = 0 )
+        response = self.client.receive_message(QueueUrl=self.url, MaxNumberOfMessages=10,  AttributeNames=[ 'SentTimestamp' ])
+        if 'Messages' not in response: return
         for msg in response['Messages']:
+            # TODO validate request as per schema
             body = ujson.loads(msg['Body'])
             data = body['Message']
             temp = ujson.loads(data)
@@ -98,7 +154,7 @@ class SearchQueue:
             self.events.append(event)
 
     def process_author(self, action, author_id, kwargs):
-        kwargs['author_id'] = author_id
+        kwargs['authorId'] = author_id
         author = Author(kwargs)
         print "encountered author ", action, kwargs
         eval("{}.{}()".format("author", action.lower()))
@@ -126,15 +182,16 @@ class SearchQueue:
                 self.process_pratilipi(action, event.resource, event.message)
             self.client.delete_message( QueueUrl=self.url, ReceiptHandle=event.rcpthandle )
 
-
 print "worker started listening for events...."
 while True:
-    print "poll queue...."
-    event_q = SearchQueue()
-    event_q.poll()
+    try:
+        print "poll queue...."
+        event_q = SearchQueue()
+        event_q.poll()
 
-    if len(event_q.events) > 0:
-        event_q.process()
-    print "sleeping now...."
-    time.sleep(30)
-
+        if len(event_q.events) > 0:
+            event_q.process()
+        print "sleeping now...."
+        time.sleep(POLL_SLEEP_TIME)
+    except Exception as err:
+        print "ERROR - sqs polling failed, {}".format(err)
